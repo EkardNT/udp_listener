fb8671b3c7197c3b89426ff6153ce570700cb08d
Drake Tetreault <ekardnt@ekardnt.com>
2020-11-10 19:18:35 -0800

    Print help if no sockets are passed.

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,8 @@ use std::thread::{spawn, JoinHandle};
 fn main() {
     let mut args: Vec<String> = std::env::args().skip(1).collect();
 
-    // **Example 1**: Bind to any IPv4 interface on the specific port 12345, in text mode.
-
-    // ```udp_listener '0.0.0.0:12345'```
-
-    // **Example 2**: Bind to any IPv6 address on a random port, in binary mode.
-
-    // ```udp_listener -b '[::]:0'```
-
-    // **Example 3**: Bind to both an IPv4 and IPv6 address, each with the same port number, in text mode.
-
-    // ```udp_listener '0.0.0.0:12345' '[::]:12345'```
-
     if args.iter().any(|arg| arg == "help" || arg == "-h" || arg == "--help") {
-        println!("udp_listener [-b|--binary] [<SOCKET_ADDR>]*");
-        println!("[Example] Bind to any IPv4 interface on port 12345: udp_listener 0.0.0.0:12345");
-        println!("[Example] Bind to any IPv6 address on a random port, in binary mode: udp_listener -b '[::]:0'");
-        println!("[Example] Bind to both IPv4 and IPv6 addresses, each with the same port number: udp_listener '0.0.0.0:12345' '[::]:12345'");
+        show_help();
         return;
     }
 
@@ -74,23 +59,27 @@ fn main() {
 
     // If there's only one, then we can be a bit more efficient by pre-locking the stdout,
     // otherwise we need to use the shared version.
-    if sockets.len() == 1 {
-        let stdout = std::io::stdout();
-        let mut stdout = stdout.lock();
-        receive_and_forward(binary, sockets.remove(0), &mut stdout);
-    } else {
-        let join_handles: Vec<JoinHandle<()>> = sockets
-            .into_iter()
-            .map(|socket| {
-                spawn(move || {
-                    let stdout = std::io::stdout();
-                    receive_and_forward(binary, socket, stdout);
+    match sockets.len() {
+        0 => show_help(),
+        1 => {
+            let stdout = std::io::stdout();
+            let mut stdout = stdout.lock();
+            receive_and_forward(binary, sockets.remove(0), &mut stdout);
+        },
+        _ => {
+            let join_handles: Vec<JoinHandle<()>> = sockets
+                .into_iter()
+                .map(|socket| {
+                    spawn(move || {
+                        let stdout = std::io::stdout();
+                        receive_and_forward(binary, socket, stdout);
+                    })
                 })
-            })
-            .collect();
-        for handle in join_handles {
-            if let Err(err) = handle.join() {
-                eprintln!("Thread panicked: {:#?}", err);
+                .collect();
+            for handle in join_handles {
+                if let Err(err) = handle.join() {
+                    eprintln!("Thread panicked: {:#?}", err);
+                }
             }
         }
     }
@@ -122,4 +111,11 @@ fn receive_and_forward(binary: bool, input: UdpSocket, mut output: impl std::io:
             }
         }
     }
+}
+
+fn show_help() {
+    println!("udp_listener [-b|--binary] [<SOCKET_ADDR>]*");
+    println!("[Example] Bind to any IPv4 interface on port 12345: udp_listener 0.0.0.0:12345");
+    println!("[Example] Bind to any IPv6 address on a random port, in binary mode: udp_listener -b '[::]:0'");
+    println!("[Example] Bind to both IPv4 and IPv6 addresses, each with the same port number: udp_listener '0.0.0.0:12345' '[::]:12345'");
 }
