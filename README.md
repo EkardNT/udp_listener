fd8b014665508e80bed1dd7de0ada76444814a07
Drake Tetreault <ekardnt@ekardnt.com>
2020-11-10 19:04:02 -0800

    Refactored to allow multiple bind addresses.

--- README.md
+++ README.md
@@ -5,15 +5,28 @@ writes the received data to standard output. On startup, the program prints the 
 local bind address to standard error. This is useful for when you don't specify a port to
 bind to, so that you can see the OS-chosen port.
 
-## Arguments
+## Usage
 
-**`-a <ADDR>`, `--address <ADDR>`**: The IP address to bind to. If not specified, binds to
-`[::]` by default. You may specify either an IPv4 or an IPv6 address.
-
-**`-p <PORT>`, `--port <PORT>`**: The UDP port to bind to. If not specified, the OS will
-choose a random free port. The local bind address is printed on program startup so that
-you can view the randomly chosen port.
+`udp_listener [-b|--binary] [<SOCKET_ADDR>]*`
 
 **`-b`, `--binary`**: If present, then the received packet data will be written to standard
 out unprocessed. If absent, then the received packet data will first be lossily parsed into
 UTF-8 text using the U+FFFD REPLACEMENT CHARACTER (�), and only then be written out.
+
+**[<SOCKET_ADDR>]\***: Zero or more socket addresses which will be bound to. A socket address
+consists of both an IP address and a port number. Both IPv4 and IPv6 are supported. Use port
+0 to have the OS assign a random free port.
+
+## Examples
+
+**Example 1**: Bind to any IPv4 interface on the specific port 12345, in text mode.
+
+```udp_listener '0.0.0.0:12345'```
+
+**Example 2**: Bind to any IPv6 address on a random port, in binary mode.
+
+```udp_listener -b '[::]:0'```
+
+**Example 3**: Bind to both an IPv4 and IPv6 address, each with the same port number, in text mode.
+
+```udp_listener '0.0.0.0:12345' '[::]:12345'```
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,67 @@
+use std::thread::{spawn, JoinHandle};
+use std::net::{AddrParseError, SocketAddr, UdpSocket};
+
 fn main() {
     let mut args: Vec<String> = std::env::args().skip(1).collect();
-    let addr = take_arg_value(&mut args, "-a", "--address").unwrap_or_else(|| "0.0.0.0".to_string());
-    let port = take_arg_value(&mut args, "-p", "--port").unwrap_or_else(|| "0".to_string());
-    let binary = args.iter().any(|arg| arg == "-b")
-        || args.iter().any(|arg| arg == "--binary");
+    let binary = args.iter().position(|arg| arg == "-b" || arg == "--binary")
+        .map(|index| args.remove(index))
+        .is_some();
 
-    // Write to standard output.
-    let stdout = std::io::stdout();
-    let mut stdout = stdout.lock();
-
-    // Bind socket.
-    let socket = match std::net::UdpSocket::bind(format!("{}:{}", addr, port)) {
-        Ok(socket) => socket,
+    // All remaining args represent socket addresses. First, resolve them all into
+    // structured addresses.
+    let mut sockets: Vec<UdpSocket> = match args.into_iter()
+        .map(|text_addr| text_addr.parse::<SocketAddr>()
+            .map_err(|err| format!("Failed to parse argument {} as socket address: {:#?}", text_addr, err))
+            .and_then(|socket_addr| UdpSocket::bind(socket_addr)
+                .map_err(|err| format!("Failed to bind to UDP address {:?}: {:#?}", socket_addr, err))
+            )
+        )
+        .collect() {
+        Ok(sockets) => sockets,
         Err(err) => {
-            eprintln!("Bind failed: {:#?}", err);
+            eprintln!("{}", err);
             return;
         }
     };
 
-    // Print out bind addr. The user might not have provided e.g. a port, in which case the OS
-    // would have assigned a random available one.
-    match socket.local_addr() {
-        Ok(local_addr) => eprintln!("Bound to {}", local_addr),
-        Err(err) => {
-            eprintln!("Local addr retrieval failed: {:#?}", err);
-            return;
+    // Print out bind addr for each socket.
+    for socket in &sockets {
+        match socket.local_addr() {
+            Ok(local_addr) => eprintln!("Bound to {}", local_addr),
+            Err(err) => {
+                eprintln!("Local addr lookup failed: {:#?}", err);
+                return;
+            }
         }
     }
 
-    // Read and dump to stdout.
+    // If there's only one, then we can be a bit more efficient by pre-locking the stdout,
+    // otherwise we need to use the shared version.
+    if (sockets.len() == 1) {
+        let stdout = std::io::stdout();
+        let mut stdout = stdout.lock();
+        receive_and_forward(binary, sockets.remove(0), &mut stdout);
+    } else {
+        let join_handles: Vec<JoinHandle<()>> = sockets.into_iter()
+            .map(|socket| {
+                spawn(move || {
+                    let stdout = std::io::stdout();
+                    receive_and_forward(binary, socket, stdout);
+                })
+            })
+            .collect();
+        for handle in join_handles {
+            if let Err(err) = handle.join() {
+                eprintln!("Thread panicked: {:#?}", err);
+            }
+        }
+    }
+}
+
+fn receive_and_forward(binary: bool, input: UdpSocket, mut output: impl std::io::Write) {
     let mut buf = [0u8; 1024 * 1024];
     loop {
-        let (byte_count, _peer_addr) = match socket.recv_from(&mut buf[..]) {
+        let (byte_count, _peer_addr) = match input.recv_from(&mut buf[..]) {
             Ok(data) => data,
             Err(err) => {
                 eprintln!("Receive failed: {:#?}", err);
@@ -39,30 +69,19 @@ fn main() {
             }
         };
 
-        // If binary, do a raw write to stdout. Otherwise convert to a string, replacing
+        // If binary, do a raw write to the output. Otherwise convert to a string, replacing
         // invalid UTF-8 with a replacement char.
         if binary {
-            if let Err(err) = std::io::Write::write_all(&mut stdout, &buf[..byte_count]) {
+            if let Err(err) = output.write_all(&buf[..byte_count]) {
                 eprintln!("Binary write to stdout failed: {:#?}", err);
                 return;
             }
         } else {
             let text = String::from_utf8_lossy(&buf[..byte_count]);
-            if let Err(err) = std::io::Write::write_all(&mut stdout, text.as_bytes()) {
+            if let Err(err) = output.write_all(text.as_bytes()) {
                 eprintln!("Text write to stdout failed: {:#?}", err);
                 return;
             }
         }
     }
-}
-
-fn take_arg_value<'args>(args: &'args mut Vec<String>, short_arg: &str, long_arg: &str) -> Option<String> {
-    args.iter()
-        .position(|arg| arg == short_arg || arg == long_arg)
-        .map(|arg_index| {
-            // Discard the arg name.
-            args.remove(arg_index);
-            // Return the arg value.
-            args.remove(arg_index)
-        })
 }
